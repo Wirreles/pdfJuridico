db82fd79fd9057af0ae15072f3cbbd071b104a5a
joelch97 <125751366+Wirreles@users.noreply.github.com>
2024-05-29 12:35:35 -0300

    aver

--- pdfJuridico/src/main.ts
+++ pdfJuridico/src/main.ts
@@ -1,12 +1,38 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { AppModule } from './app/app.module';
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes),
+    importProvidersFrom(
+     provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()),
+      provideAnalytics(() => getAnalytics())
+    ),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    ScreenTrackingService,
+    UserTrackingService, provideFirebaseApp(() => initializeApp({"projectId":"pdf-manager-7fbf8","appId":"1:775859610688:web:af2da5023833562c5eeba5","storageBucket":"pdf-manager-7fbf8.appspot.com","locationId":"us-west1","apiKey":"AIzaSyCFQ1v-2lDvQyCidenRGnREeLyml5s84a8","authDomain":"pdf-manager-7fbf8.firebaseapp.com","messagingSenderId":"775859610688","measurementId":"G-DVHFJGDYQ1"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage())
+  ],
+});
